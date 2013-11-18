bbdeed4a4101e9af84320fb350b55e2c008d1f64
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-11-17 23:51:35 -0800

    use simple_cycles2 (even though it's still problematic)

--- process_links.py
+++ process_links.py
@@ -7,6 +7,23 @@ import networkx as nx
 from networkx import simple_cycles
 
 
+def simple_cycles2(G):
+    comps = nx.strongly_connected_component_subgraphs(G)
+    print 'found %s strongly-connected component subgraphs' % len(comps)
+    all_cycles = []
+    do_pdb = True
+    for c in comps:
+        if len(c) == 1:
+            continue
+        else:
+            for cycle in nx.simple_cycles(c):
+                print 'found cycle, length: %s' % len(cycle)
+                all_cycles.append(cycle)
+                if len(all_cycles) % 10 == 0 and do_pdb:
+                    import pdb;pdb.set_trace()
+    return all_cycles
+
+
 def main(link_json_filename):
     cat_links = json.load(open(link_json_filename))
     graph = nx.DiGraph()
@@ -14,7 +31,8 @@ def main(link_json_filename):
         graph.add_edge(*link)
         if i % 100000 == 0:
             print i, 'links loaded'
-    cyc_iter = simple_cycles(graph)
+    del cat_links
+    cycles = simple_cycles2(graph)
     import pdb;pdb.set_trace()
 
 
